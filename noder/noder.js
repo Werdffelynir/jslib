e05054238ef0718ecfd8a7bda13b46533d9017a1
werdffelynir <werdffelynir@gmail.com>
2016-12-09 15:34:51 +0200

    xxx

--- noder/noder.js
+++ noder/noder.js
@@ -1,7 +1,7 @@
 (function(window){
 
     /**
-     * // Эквивалентно Noder.refresh ()
+     * // Эквивалентно Noder.refresh () + Noder.get ()
      * Noder ()
      * // Эквивалентно Noder.get (key)
      * Noder (key)
@@ -11,16 +11,16 @@
      * // Объетк глобальных параметров скрипта
      * Noder.config
      *
-     * // Повтор поиска нодов
+     * // Повтор поиска нодов в DOM и сохранение/ссылка в хранилище
      * Noder.refresh ()
      *
-     * // Добавления нода
+     * // Добавления нода в хранилище
      * Noder.add ('group', elem)
      *
-     * // Выбрать массив всех нодов
+     * // Выбрать массив всех нодов с хранилища
      * Noder.get ()
      *
-     * // Выбрать массив нодов по ключу
+     * // Выбрать массив нодов с хранилища по ключу
      * Noder.get ('group')
      *
      * // Выбрать массив нодов по ключу, если указан второй аргумент - фукция,
@@ -30,6 +30,12 @@
      * // Выбрать единственный елемент нода, если указан второй аргумент - фукция,
      * результат выборки попадет в первый параметр фукции
      * Noder.getOne ('group', function (elem) {})
+     *
+     * // Если существует "element" в хранилище или елемнт/ы в группе по ключу существуют
+     * // Выполяет функция 1, в какчестве параметра передается массив совпадений
+     * // В противном случае функция 2
+     * Noder.exist (element, function (elems) {}, function () {})
+     * Noder.exist ('group', function (elems) {}, function () {})
      *
      * // Пройдет по списку каждого нода.
      * Noder.each (function (elem) {})
@@ -49,16 +55,38 @@
      * Noder.click ('group', function (event, elem, value) {});
      *
      * // Выборка елементов по селектору
-     * Noder.query (selector, callback)
-     * Noder.queryOne (selector, callback)
+     * Noder.query (selector, function (elems) {})
+     * Noder.queryOne (selector, function (elem) {})
      *
      * // Создать новый елемент
      * Noder.create (tag, attrs, inner)
+     *
+     * // Шаблонизаыия строки viewString и объекта params
+     * // все подстроки обернутые как переменные {{var_name}},
+     * // будут заменены значчением свойства
+     * // объекта params по ключу/имени, если существует
+     * Noder.template (viewString, params)
+     *
+     * // Обходит последовательность - массивы, объекты, массиво-подобные объекты
+     * Noder.eachElement (data, callback)
+     *
+     * //
+     * Noder.style (key, styleObject)
+     *
+     * // Работа с формами
+     * Noder.Form
+     * Noder.Form.data (form)
+     * Noder.Form.getData (key)
+     * Noder.Form.elements (form)
+     * Noder.Form.getElements (key)
+     *
      **/
 
     function Noder(key, node) {
-        if (arguments.length === 0)
+        if (arguments.length === 0) {
             Noder.refresh();
+            return Noder.get ()
+        }
         else if (arguments.length === 1)
             Noder.get(key);
         else if (arguments.length === 2)
@@ -76,29 +104,46 @@
             elems = document.querySelectorAll(selector);
 
         for(var i = 0; i < elems.length; i ++ ){
-            if (key = elems[i].getAttribute(Noder.config.keyName)) {
+            if (elems[i].getAttribute(Noder.config.keyName) && Noder._nodes.indexOf(elems[i]) === -1) {
+                key = elems[i].getAttribute(Noder.config.keyName);
                 Noder.add(key, elems[i]);
             }
         }
         return Noder._nodes;
     };
-    Noder.add = function (key, elem) {
+    Noder.add = function (key, elem, update) {
         if (typeof elem === 'string') {
             elem = Noder.queryOne(elem);
         }
+
         if (elem.nodeType !== Node.ELEMENT_NODE ) {
             console.error('Error. Second argument ['+elem.toString()+'] is not node element!');
             return;
         }
+
         if (Noder._nodes.indexOf(elem) === -1) {
-            elem.key = key;
+            // todo: renamed name of property 'key' => 'noderKey'
+            elem.key = elem.noderKey = key;
             Noder._nodes.push(elem);
-        } else
-            console.error('Element ['+elem.toString()+'] was not added! It exits in storage.');
+            return Noder;
+        } else {
+            if (update) {
+                Noder.remove(key, elem);
+                return Noder.add(key, elem);
+            }
+            else
+                console.error('Element ['+elem.toString()+'] was not added! It exits in storage.');
+        }
+
     };
+
+    Noder.update = function (key, elem) {
+        return Noder.add(key, elem, true);
+    };
+
     Noder.remove = function (key, elem) {
         for(var i = 0; i < Noder._nodes.length; i ++) {
-            if(Noder._nodes[i].key === key) {
+            if(Noder._nodes[i].noderKey === key) {
                 if (elem === undefined) {
                     delete Noder._nodes[i];
                 }
@@ -108,12 +153,13 @@
                 }
             }
         }
+        return Noder;
     };
     Noder.get = function (key, callback) {
         var i, result = [], list = Noder._nodes;
         if (key) {
             for(i = 0; i < list.length; i ++) {
-                if(list[i].key === key)
+                if(list[i].noderKey === key)
                     result.push(list[i]);
             }
         } else if (arguments.length === 0) {
@@ -125,13 +171,29 @@
 
         return result;
     };
+
     Noder.getOne = function (key, callback) {
         var elem = Noder.get(key);
         if (elem.length > 0) elem = elem[0];
+        else elem = false;
         if (typeof callback === 'function')
             callback.call({}, elem);
         return elem;
     };
+    Noder.exist = function (key, ifCallback, elseCallback) {
+        var elems = [];
+        if (typeof key === 'string')                        elems = Noder.get(key);
+        else if (key && key.nodeType === Node.ELEMENT_NODE) elems = [key];
+
+        if (arguments.length > 1) {
+            if (elems.length > 0 && typeof ifCallback === 'function')
+                ifCallback.call({}, elems);
+            else if (elems.length == 0 && typeof elseCallback === 'function')
+                elseCallback.call({});
+        }
+        return elems.length > 0;
+    };
+
     Noder.each = function (key, callback, thisInst) {
         if (arguments.length === 1 && typeof key === 'function') {
             Noder._nodes.map(key, thisInst || {})
@@ -139,6 +201,7 @@
         } else if (typeof key === 'string' && typeof callback === 'function') {
             Noder.get(key).map(callback, thisInst || {});
         }
+        return Noder
     };
     Noder.on = function (event, key, callback, useCapture) {
         var elem = Noder.get(key);
@@ -147,6 +210,7 @@
                 elem[i].addEventListener(event, callback, useCapture);
             }
         }
+        return Noder
     };
     Noder.click = function (key, callback, useCapture) {
         var valueName = Noder.config.valueName;
@@ -154,6 +218,7 @@
             callback.call(event, event, event.target, event.target.getAttribute(valueName))
         };
         Noder.on('click', key,  specialCallback, useCapture);
+        return Noder
     };
     Noder.query = function (selector, callback) {
         var elems = Array.prototype.slice.call(document.querySelectorAll(selector));
@@ -183,6 +248,58 @@
                 viewString = viewString.replace(new RegExp('{{' + k + '}}', 'gi'), params[k]);
         return viewString;
     };
+
+    Noder.eachElement = function (data, callback) {
+        if(data && data.length > 0) {
+            for(var i = 0; i < data.length; i ++) callback.call(data, data[i], i);
+        }else if(Object.prototype.toString.call(data) === '[object Object]'){
+            for(var k in data) callback.call(data, data[k], k);
+        }
+    };
+
+    Noder.style = function (key, styleObject) {
+        var k, elems = [];
+
+        if (typeof key === 'string')                        elems = Noder.get(key);
+        else if (key && key.nodeType === Node.ELEMENT_NODE) elems = [key];
+
+        elems.map (function (elem) {
+            for (k in styleObject)
+                elem.style[k] = styleObject[k]
+        });
+    };
+
+
+    //////////////////////////////////////////  Noder.Form   ////////////////////////////////////////////
+
+    Noder.Form = {};
+
+    Noder.Form.getData = {};
+
+    Noder.Form.data = {};
+
+    Noder.Form.getElements = function (key) {
+        return Noder.Form.elements(Noder.getOne(key));
+    };
+
+    Noder.Form.elements = function (form) {
+        var i, elem, node, resultObject = {asString: ''};
+
+        for (i = 0; i < form.length; i++) {
+            elem = form[i];
+            node = elem.nodeName.toLowerCase();
+
+            if (!resultObject[elem.name])
+                resultObject[elem.name] = elem;
+
+            if (['input','select','textarea'].indexOf(node) !== -1)
+                resultObject[elem.name].asString += ((resultObject[elem.name].asString == '') ? '' : '&') + elem.name + '=' + encodeURIComponent(elem.value);
+        }
+        return resultObject
+    };
+
+
+
 
     window.Noder = Noder;
     window.Noder.version = '0.0.1'
