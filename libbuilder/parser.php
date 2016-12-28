279904133b13b751b608d1d971ce835cade37cd6
werdffelynir <werdffelynir@gmail.com>
2016-12-28 13:59:08 +0200

    add lib builder

--- libbuilder/parser.php
+++ libbuilder/parser.php
@@ -21,8 +21,7 @@ if (!is_file($root . 'config.json')) {
 }
 
 $config = json_decode(file_get_contents($root . 'config.json'), true);
-
-$root = $config['root'];
+$rootModules = $config['root'];
 $layoutPath = $config['layout'];
 $modules = $config['modules'];
 $replaces = $config['replaces'];
@@ -30,11 +29,10 @@ $delete_string_marker = $config['delete_string_marker'];
 $layout = file_get_contents($layoutPath);
 
 foreach($modules as $mod => $file) {
-    if(is_file($root.$file) && $ctx = file_get_contents($root.$file)) {
-        //$ctx =  "\n// --------------------------------------------------------- {$mod} --- //\n" . $ctx . "\n";
+    if(is_file($rootModules.$file) && $ctx = file_get_contents($rootModules.$file))
         $layout = str_replace("[[['$mod']]]", $ctx, $layout);
-    } else
-        show("[Parser] Error. File $root.$file not find.");
+    else
+        show("[Parser] Error. File $rootModules.$file not find.");
 }
 
 // replace
@@ -53,33 +51,8 @@ foreach ($layoutArr as $i => $str) {
 
 $layout = join("\n", $layoutArr);
 
-
-
-
 // build
 if (file_put_contents($config['dump'], $layout)) {
     show("[Parser] File {$config['dump']} is created.");
 } else
     show("[Parser] Error File {$config['dump']} not created. Content not find.");
-
-/*
-$dataSources = '';
-foreach($config['prepare'] as $s) {
-    $f = $config['root'] . $s;
-    if (is_file($f)) {
-        $c = file_get_contents($f);
-        $dataSources .= "\n// --------------------------------------------------------- {$f} --- //\n" . $c . "\n";
-    }
-    else
-        show("[Parser] Error. File $f not find.");
-}
-
-// build
-if (file_put_contents($config['dump'], $dataSources)) {
-    show("[Parser] File {$config['dump']} is created.");
-} else
-    show("[Parser] Error File {$config['dump']} not created. Content not find.");
-
-//show($dataSources);
-
-//print_r($config);*/
