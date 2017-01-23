a22585cc4d1297bb48a8d51da4d28dca9cccee5e
werdffelynir <werdffelynir@gmail.com>
2017-01-23 17:18:53 +0200

    xxx

--- ultree/ultree.js
+++ ultree/ultree.js
@@ -26,18 +26,12 @@
         for (i = 0; i < root.length; i ++)
             this._internal_scan(root[i], this._internal_structure);
 
-        // customize tree style
-        this.customization();
-
-        // run parameters
-        if (this.config.children_hide)
-            this.hideChildren();
-        else
-            this.showChildren();
+        // set active buttons for open tree
+        this._create_actions();
     };
 
     ultree.prototype._internal_scan = function(element, structure) {
-        var i, item, index, className, children = [];
+        var i, before, item, index, className, children = [];
 
         className = 'item_' + this._internal_structure_iterator;
         element.classList.add(className);
@@ -54,23 +48,36 @@
         index = structure.indexOf(item);
         children = this.queryAll('.' + className + '>ul>li', element);
 
-        if (children) {
+        if (children.length > 0) {
             for (i = 0; i < children.length; i ++)
                 this._internal_scan(children[i], structure[index].children);
+
+            // add elem arrow
+            before = document.createElement('i');
+            before.setAttribute('data-before', 'arrow');
+            element.parentNode.insertBefore(before, element);
+
+        } else {
+
+            // add elem item icon
+            before = document.createElement('i');
+            before.setAttribute('data-before', 'item');
+            element.parentNode.insertBefore(before, element);
+
         }
     };
 
-    ultree.prototype.customization = function() {
-        return this._internal_structure
+    ultree.prototype._create_actions = function() {
+        var i, parent, arrows = document.querySelectorAll('i[data-before="arrow"]');
+        for (i = 0; i < arrows.length; i ++) {
+            arrows[i].addEventListener('click', onToggleChildren);
+        }
+
     };
 
     ultree.prototype.getStructure = function() {
         return this._internal_structure
     };
-
-    ultree.prototype.showChildren = function() {};
-
-    ultree.prototype.hideChildren = function() {};
 
     ultree.prototype.query = function(selector, from) {
         from = from && from.nodeType === Node.ELEMENT_NODE ? from : document;
@@ -81,6 +88,24 @@
         from = from && from.nodeType === Node.ELEMENT_NODE ? from : document;
         return Array.prototype.slice.call( from.querySelectorAll(selector) );
     };
+
+    function onToggleChildren(event) {
+        var target = event.target,
+            parent = target.nextSibling,
+            parentUL = parent && parent.tagName == 'LI' ? parent.querySelector('ul') : false;
+
+        if (parentUL) {
+            if (parentUL.style.display == 'none'){
+                parentUL.style.display = 'block';
+                target.style.backgroundPosition = '-36px -8px';
+            }
+            else{
+                parentUL.style.display = 'none';
+                target.style.backgroundPosition = '0px -8px';
+            }
+        }
+
+    }
 
     window.ULTree = ultree;
 })();
