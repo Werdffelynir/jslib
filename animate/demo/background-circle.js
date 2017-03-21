e837f7bd35af7599baa93798033315baaeffc9b3
Werdffelynir <werdffelynir@gmail.com>
2017-03-21 12:08:30 +0200

    dev

--- animate/demo/background-circle.js
+++ animate/demo/background-circle.js
@@ -15,7 +15,8 @@
     });
 
     // * * * * * * * * * * * * * * * * * * * * * * * * *
-    Game = {img:{}, bgFileName:null};
+    Game = {img:{}};
+    /*
     Game.movieBg = an.createClip({
         speed: 1.5,
         img1_x: 0,
@@ -24,7 +25,6 @@
         img2_y: 0,
         correct_x: 0
     }, function (ctx, i) {
-        /** @type CanvasRenderingContext2D */
         ctx = ctx;
         var img = Game.img[Game.bgFileName];
         if (!img) return;
@@ -40,28 +40,72 @@
 
         if (this.img1_x < -img.width + this.correct_x ) this.img1_x = img.width + this.correct_x ;
         if (this.img2_x < -img.width + this.correct_x ) this.img2_x = img.width + this.correct_x ;
-    });
+    });*/
 
+    Game.createBgClip = function (image, x, y, speed, correct) {
+        return an.createClip({
+            speed: speed || 2,
+            img1_x: x,
+            img1_y: y,
+            img2_x: false,
+            img2_y: y,
+            correct_x: correct || 0
+        }, function (ctx, i) {
+            /** @type CanvasRenderingContext2D */
+            ctx = ctx;
+            if (this.img2_x === false)
+                this.img2_x = image.width + this.correct_x ;
+
+            ctx.drawImage(image, this.img1_x, this.img1_y);
+            ctx.drawImage(image, this.img2_x, this.img2_y);
+
+            this.img1_x -= this.speed;
+            this.img2_x -= this.speed;
+
+            if (this.img1_x < -image.width + this.correct_x ) this.img1_x = image.width + this.correct_x ;
+            if (this.img2_x < -image.width + this.correct_x ) this.img2_x = image.width + this.correct_x ;
+                             
+            return this;
+        });
+    };
 
     an.frame('game', function(ctx, i){
         /** @type CanvasRenderingContext2D */
         ctx = ctx;
 
-        Game.movieBg(ctx, i);
+        Game.bg3(ctx, i);
+        Game.bg2(ctx, i);
+        Game.bg1(ctx, i);
+        //Game.movieBg(ctx, i);
 
     });
 
     NSA.domLoaded(function () {
-        an.Image.load({
-            bg_run: '/animate/demo/images/bg_pixel.jpg',
-            zip3: '/animate/demo/images/zip3.png'
-        }, function (list) {
-            Game.img = list;
+        an.resource.loadImage({
+            bg1: '/animate/demo/images/bgparallax/1.png',
+            bg2: '/animate/demo/images/bgparallax/2.png',
+            bg3: '/animate/demo/images/bgparallax/3.png',
+            flying: '/animate/demo/images/flying.png'
+        }, function (imgs) {
+            Game.img = imgs;
 
-            Game.heroStat.image = Game.img['zip3'];
-
+            console.log(imgs);
+            
+            Game.bg1 = Game.createBgClip(imgs['bg1'], 0, 150, 5, -5);
+            Game.bg2 = Game.createBgClip(imgs['bg2'], 0, 50, 3, 0);
+            Game.bg3 = Game.createBgClip(imgs['bg3'], 0, 0, 1, 0);
+            
+            //Game.heroStat.image = Game.img['zip3'];
+            
+            
+            
+            
+            
+            
+            
+            // start
+            an.start('game');
         });
     });
-    // start
-    an.start('game');
+    
 })();
