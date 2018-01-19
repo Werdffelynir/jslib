61b3d9d152640b3812ecb81ea1db47842c78bb72
Werdffelynir <werdffelynir@gmail.com>
2018-01-19 18:14:42 +0200

    drawline

--- animate/demo/graphic/draw-line.js
+++ animate/demo/graphic/draw-line.js
@@ -4,49 +4,151 @@
     selector: '#canvas',
     width: 600,
     height: 400,
-    fps: 12,
-
-    // Movies
-    act: 'start',
-    acts: {start: 'start', b: ''}
+    fps: 1
   });
 
   // * * * * * * * * * * * * * * * * * * * * * * * * *
   var Graphic = An.Graphic();
   var TextField = An.TextField();
 
-  var DrawLine = Animate.Clip({
-    animated: true,
-    ep: false,
-    speed: 1
-  }, function (sp, ep, s) {
+  // * * * * * * * * * * * * * * * * * * * * * * * * *
+  var Movie = {
+    player: {},
+    drawline: {},
 
-    if (!this.ep) this.ep = sp;
+  };
 
-    if (this.animated) {
-      var f = function (n1, n2) {
-        return n1 < n2 ? 1 : -1;
-      };
+  // var sPoint = {
+  //   x: 10,
+  //   y: 10
+  // };
+  //
+  // var ePoint = {
+  //   x: 100,
+  //   y: 300
+  // };
+  // var dPoint = {
+  //   x: 0,
+  //   y: 0
+  // };
 
-      this.ep.x += f(sp.x, ep.x);
-      this.ep.y += f(sp.y, ep.y);
+  var Player = An.Clip({
+    // x: 0,
+    // y: 0,
+    // dx: 0,
+    // dy: 0,
+    // moveToX: 0,
+    // moveToY: 0,
+    // speed: 3,
+    // acceleration: 0.1
+  }, function (ctx, i) {
 
-      console.log(this.ep);
+    // this.dx = (this.moveToX - this.x) / 50;
+    // this.dy = (this.moveToY - this.y) / 50;
 
-      Graphic.line(sp.x, sp.y, this.ep.x, this.ep.y)
-        .stroke();
+    // if (An.distanceBetween(Animate.Point(this.x, this.y), Animate.Point(this.moveToX, this.moveToY)) > 5) {
+    //   this.x += this.dx;
+    //   this.y += this.dy;
+    // }
+
+    /*
+    if (this.x > this.dx) {
+      this.dx += this.speed;
     }
+    if (this.x < this.dx) {
+      this.dx -= this.speed;
+    }
+
+    if (this.y > this.dy) {
+      this.dy += this.speed;
+    }
+    if (this.y < this.dy) {
+      this.dy -= this.speed;
+    }*/
+
+    // Graphic.circle(this.x, this.y, 25)
+    //   .thickness(5)
+    //   .stroke();
+
+    return this;
   });
 
-  // var line_1 = DrawLine(Animate.Point(0, 0), Animate.Point(100, 100));
-  // console.log(R1.start());
 
-  An.frame(function (ctx, i) {
+
+  var getDistance = function (a, b) {
+    return Math.sqrt((b.x - a.x) * (b.x - a.x) + (b.y - a.y) * (b.y - a.y));
+  };
+
+  var DrawLine = An.Clip({
+    startX: 300,
+    startY: 10,
+    toX: 300,
+    toY: 300,
+    dX: null,
+    dY: null,
+    speed: 3
+  }, function (ctx, i) {
+
+    if (this.dX === null && this.dY === null) {
+      this.dX = this.startX;
+      this.dY = this.startY;
+    }
+
+    // var dist = getDistance(
+    //   Animate.Point(this.startX, this.startY),
+    //   Animate.Point(this.toX, this.toY)
+    // );
+    //
+    // var angle = Animate.calculateAngle(
+    //   Animate.Point(this.startX, this.startY),
+    //   Animate.Point(this.toX, this.toY)
+    // );
+    //
+    //
+
+    this.dX = (this.toX - this.dX) / 5;
+    this.dY = (this.toY - this.dY) / 5;
+
+
+    console.log(this);
+
+    Graphic.line(this.startX, this.startY, this.dX, this.dY)
+      .thickness(5)
+      .stroke();
+
+    return this;
+  });
+
+    An.frame(function (ctx, i) {
     /** @type CanvasRenderingContext2D */
     ctx = ctx;
 
-    DrawLine(Animate.Point(0, 0), Animate.Point(300, 100));
+      Movie.drawline = DrawLine();
 
+
+    // Movie.player = Player();
+    // console.log(Movie.player);
+    // var deltaTime = (i - startTime) / duration;
+    // dPoint.x += (ePoint.x - sPoint.x) / 40;
+    // dPoint.y += (ePoint.y - sPoint.y) / 40;
+    // Graphic.line(
+    //   sPoint.x, sPoint.y,
+    //   ePoint.x, ePoint.y
+    // ).stroke();
+
+  });
+
+  An.onClick(function (event, pos) {
+
+    // dPoint = sPoint;
+    // ePoint = pos;
+    // Movie.player.dx = Math.cos((pos.x - Movie.player.x));
+    // Movie.player.dy = Math.sin((pos.y - Movie.player.y));
+    // Movie.player.dx = (pos.x - Movie.player.x) / 50;
+    // Movie.player.dy = (pos.y - Movie.player.y) / 50;
+    //
+    // Movie.player.moveToX = pos.x;
+    // Movie.player.moveToY = pos.y;
   });
 
   // start
